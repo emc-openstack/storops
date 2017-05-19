3f4cdc0d282272cfca5e6aaaaac865c1db48c44c
Ryan Liang <ryan.liang@emc.com>
2017-05-19 16:49:15 +0800

    Export TCHelper

--- storops/__init__.py
+++ storops/__init__.py
@@ -16,8 +16,8 @@
 from __future__ import unicode_literals
 
 import sys
-import logging
 
+from storops.lib.thinclone_helper import TCHelper
 from storops.unity.enums import *  # noqa
 from storops.unity.resource.system import UnitySystem  # noqa
 from storops.vnx.enums import *  # noqa
